c3bcab43a4f8d3d3210b225771e2030f50036381
andmironov <thatsmironov@gmail.com>
2016-01-12 02:40:25 +0300

    added so header footer sidebar styles

--- prototypes/payments-dashboard/webpack.production.config.js
+++ prototypes/payments-dashboard/webpack.production.config.js
@@ -17,9 +17,14 @@ var config = {
       test: /\.jsx?$/,
       exclude: [node_modules_dir],
       loader: 'babel-loader'
-    },{
-      test: /\.less$/,
-      loader: 'style!css!less'
+    },
+    {
+      test: /\.(eot|woff|woff2|ttf)$/,
+      loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
+    },
+    {
+      test: /\.scss$/,
+      loader: 'style!css!sass'
     },
     {
         test: /\.(jpe?g|png|gif|svg)$/i,
