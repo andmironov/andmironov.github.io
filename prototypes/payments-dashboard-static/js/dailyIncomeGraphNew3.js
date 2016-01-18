afe96f6a51135f1ac14eebe9a5742dcf5b298c37
andmironov <thatsmironov@gmail.com>
2016-01-18 19:03:16 +0300

    added income graph

--- prototypes/payments-dashboard-static/js/dailyIncomeGraphNew3.js
+++ prototypes/payments-dashboard-static/js/dailyIncomeGraphNew3.js
@@ -1,24 +1,22 @@
 var d3 = require("./lib/d3.js");
-
-
 var dataset = [
                 ["2015-12-31 00:02", 600],
-
-                ["2016-01-01 00:02", 900],
-                ["2016-01-01 00:03", 1900],
-                ["2016-01-01 04:04", 940],
+                ["2016-01-01 03:44", 900],
+                ["2016-01-01 06:03", 100],
+                ["2016-01-01 10:04", 940],
                 ["2016-01-01 12:50", 1300],
                 ["2016-01-01 14:00", 1100],
-                ["2016-01-01 15:00", 900],
+                ["2016-01-01 15:00", 1300],
                 ["2016-01-01 15:30", 400],
 
                 ["2016-01-02 00:13", 200],
                 ["2016-01-02 03:30", 1000],
                 ["2016-01-02 09:50", 1100],
-                ["2016-01-02 13:10", 1800],
+                ["2016-01-02 13:10", 900],
                 ["2016-01-02 18:10", 1400]
               ];
 
+var interpolation = "basis";
 var parseTime = d3.time.format.utc("%Y-%m-%d %H:%M").parse;
 
 var yesterdayMidnight = parseTime("2016-01-01 00:00");
@@ -40,9 +38,12 @@ var xScale = d3.time.scale.utc()
                           .domain([todayMidnight, todayBeforeMidnight])
                           .range([0, graphWidth ])
 
+var initialYScale = d3.scale.linear()
+                      .domain([100, d3.max(dataset, function(d) { return d[1] + 100 })])
+                      .range([graphHeight, graphHeight]);
 
 var yScale = d3.scale.linear()
-                     .domain([100, d3.max(dataset, function(d) { return d[1] })])
+                     .domain([100, d3.max(dataset, function(d) { return d[1] + 100 })])
                      .range([graphHeight - graphTopPadding - graphBottomPadding, 1]);
 
 var yGrid = d3.svg.axis()
@@ -73,18 +74,15 @@ var svg = d3.select(".graph--daily-income-graph .graph__data-visualisation")
 var area = d3.svg.area()
                  .x(function(d) { return xScale(parseTime(d[0])) })
                  .y0(graphHeight)
-                 .y1(function(d) { return yScale(d[1]) })
-                 .interpolate("basis");
+                 .y1(function(d) { return initialYScale(d[1]) })
+                 .interpolate(interpolation);
 
 var line = d3.svg.line()
-                .interpolate("basis")
                 .x(function(d) { return xScale(parseTime(d[0])) })
-                .y(function(d) { return yScale(d[1]) });
+                .y(function(d) { return initialYScale(d[1]) })
+                .interpolate(interpolation);
 
-
-
-
-svg.datum(dataset);
+//svg.datum(dataset);
 
 //Add clippath for the grid
 svg.append("clipPath")
@@ -93,35 +91,50 @@ svg.append("clipPath")
    .attr("width", graphWidth - 70)
    .attr("height", graphHeight - 70);
 
-//Add points
-/*
-var points = svg.append("g")
-                .attr("class", "graph__points")
-                .attr("width", graphWidth)
-                .attr("height", graphHeight);
+// Hover line
+var hoverLineGroup = svg.append("g")
+					              .attr("class", "graph__hover-line");
 
-points.selectAll('circle')
-        .data(dataset.filter(function(d) { return d; }))
-        .enter().append('circle')
-        .attr('cx', line.x())
-        .attr('cy', line.y())
-        .attr('r', 3)
-*/
+var hoverLine = hoverLineGroup.append("line")
+                          		.attr("x1", -1)
+                              .attr("x2", -1)
+                          		.attr("y1", graphTopPadding)
+                              .attr("y2", (graphHeight))
+                              .style("opacity", 0);
 
-//Draw graph
+//Draw axes
+svg.on("mousemove", function (d, i) {
+       var m = d3.mouse(this);
+       hoverLine.attr("x1", m['0'])
+                .attr("x2", m['0'])
+                .style("opacity", .4)
+   })
+
+svg.on("mouseout", function() { hoverLine.style("opacity", 0)});
+
+svg.append("g")
+  .attr("class", "graph__axis graph__axis--x")
+  .attr("transform", "translate(" + xAxisPadding[3] + ", " + (graphHeight - xAxisPadding[2]) + ")")
+  .call(xAxis);
+
+svg.append("g")
+  .attr("class", "graph__axis graph__axis--y")
+  .attr("transform", "translate(" + (graphWidth - xAxisPadding[1]) + ", " + (xAxisPadding[0] + graphTopPadding) + ")")
+  .style("text-anchor", "end")
+  .call(yAxis);
+
+//Draw area
 svg.append("path")
    .attr("class", "graph__area")
-   .attr("d", area)
-   .on("mousemove", function (d, i) {
-            var m = d3.mouse(this);
-            console.log(m);
-          });
+   .datum(dataset)
+   .attr("d", area);
 
 //Draw line
-/*svg.append("path")
-      .attr("class", "graph__line")
-      .attr("d", line);
-*/
+svg.append("path")
+   .attr("class", "graph__line")
+   .datum(dataset)
+   .attr("d", line);
+
 //Draw grid
 svg.append("g")
    .attr("class", "graph__grid graph__grid--y")
@@ -130,22 +143,7 @@ svg.append("g")
    .style("text-anchor", "end")
    .call(yGrid);
 
-//Draw axes
-svg.append("g")
-   .attr("class", "graph__axis graph__axis--x")
-   .attr("transform", "translate(" + xAxisPadding[3] + ", " + (graphHeight - xAxisPadding[2]) + ")")
-   .call(xAxis);
-
-svg.append("g")
-   .attr("class", "graph__axis graph__axis--y")
-   .attr("transform", "translate(" + (graphWidth - xAxisPadding[1]) + ", " + (xAxisPadding[0] + graphTopPadding) + ")")
-   .style("text-anchor", "end")
-   .call(yAxis);
-
-
-
-//Onclick events
-
+//Events
 d3.selectAll(".data-swither")
   .on("click", function(){
 
@@ -162,7 +160,33 @@ d3.selectAll(".data-swither")
     moveGraph(direction);
   });
 
+//Draw graph with animation
+setTimeout(drawGraph, 200);
+function drawGraph() {
+    initialYScale.range([graphHeight - graphTopPadding - graphBottomPadding, 1]);
+    var t = svg.transition().duration(750);
+    t.select(".graph__area").attr("d", area);
+    t.select(".graph__line").attr("d", line);
+}
 
+//Update graph data with animation
+//setTimeout(updateGraph, 2000);
+function updateGraph() {
+  var newDataset = dataset;
+  newDataset.push(["2016-01-02 19:30", 2200]);
+  initialYScale.domain([100, d3.max(newDataset, function(d) { return d[1] + 100 })]);
+  yScale.domain([100, d3.max(newDataset, function(d) { return d[1] + 100 })]);
+
+
+  thePath.datum(newDataset);
+  var t = svg.transition().duration(750);
+  t.select(".graph__line").attr("d", line);
+  t.select(".graph__area").attr("d", area);
+  t.select(".graph__axis--y").call(yAxis);
+  t.select(".graph__grid--y").call(yGrid);
+}
+
+//Update graph on swither click with animation
 var currentPosition = "today";
 function moveGraph(direction) {
   if(direction == currentPosition) return;
@@ -178,9 +202,6 @@ function moveGraph(direction) {
   var t = svg.transition().duration(750);
   t.select(".graph__axis--x").call(xAxis);
   t.select(".graph__area").attr("d", area);
-  t.select(".graph__points").selectAll('circle')
-  .attr('cx', line.x())
-  .attr('cy', line.y())
   t.select(".graph__line").attr("d", line);
 
   return;
