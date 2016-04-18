fd126680b42e78f60a1b3fa22ed8a081f5876c15
andmironov <thatsmironov@gmail.com>
2016-04-18 12:27:55 +0300

    added minor fixes

--- prototypes/status/js/main.js
+++ prototypes/status/js/main.js
@@ -1,1 +1,41 @@
 var DOMObserver = require("./lib/DOMObserver.js")
+
+//Shake the input
+
+var button = document.querySelectorAll(".email-caption-button")[0]
+var input = document.querySelectorAll(".email-caption-input")[0];
+var inputWrap = document.querySelectorAll(".email-caption-inner")[0];
+
+button.addEventListener("click", function(){
+  shakeInput()
+})
+
+input.addEventListener("focus", function(){
+  focusInput()
+})
+
+var inputIsShaking = false
+function shakeInput() {
+  if(!inputIsShaking) {
+    inputIsShaking = true;
+    inputWrap.classList.add("shaky")
+
+    setTimeout(function(){
+      inputWrap.classList.remove("shaky")
+      inputIsShaking = false;
+    }, 800)
+  }
+}
+
+var inputIsFocusing = false
+function focusInput() {
+  if(!inputIsFocusing) {
+    inputIsFocusing = true;
+    inputWrap.classList.add("focused")
+
+    setTimeout(function(){
+      inputWrap.classList.remove("focused")
+      inputIsFocusing = false;
+    }, 400)
+  }
+}
