c7f0a1dec0d9a7d2b3a1ae853fbf658072e2eaa2
andmironov <thatsmironov@gmail.com>
2016-04-19 16:27:08 +0300

    added minor fixes

--- prototypes/status/js/main.js
+++ prototypes/status/js/main.js
@@ -3,8 +3,8 @@ var DOMObserver = require("./lib/DOMObserver.js")
 //Shake the input
 
 var button = document.querySelectorAll(".email-caption-button")[0]
-var input = document.querySelectorAll(".email-caption-input")[0];
-var inputWrap = document.querySelectorAll(".email-caption-inner")[0];
+var input = document.querySelectorAll(".email-caption-input")[0]
+var inputWrap = document.querySelectorAll(".email-caption-inner")[0]
 
 button.addEventListener("click", function(){
   shakeInput()
@@ -44,6 +44,8 @@ function focusInput() {
 var observer = new DOMObserver();
 
 var mainSlideElement = document.querySelectorAll(".slide--top")[0]
+var colorfulSlideElement = document.querySelectorAll(".slide--colorful")[0]
+var colorfulSlideBlocksElements = document.querySelectorAll(".colorful-blocks .colorful-block")
 var taglineElement = document.querySelectorAll(".slide--top .tagline")[0]
 var emailCaptionElement = document.querySelectorAll(".email-caption")[0]
 var headerElement = document.querySelectorAll(".header")[0]
@@ -53,11 +55,20 @@ observer.addElement({
   name:"mainSlide"
 })
 
+observer.addElement({
+  element: colorfulSlideElement,
+  name:"colorfulSlide"
+})
+
 
 var scrollY = observer.getScrollY()
 var viewportHeight = observer.getViewport().height
+
 var mainSlideHeight = observer.getPropertyValue("mainSlide", "height")
 var mainSlideOffsetTop = observer.getPropertyValue("mainSlide", "offsetTop")
+
+var colorfulSlideHeight = observer.getPropertyValue("colorfulSlide", "height")
+var colorfulSlideOffsetTop = observer.getPropertyValue("colorfulSlide", "offsetTop")
 
 onScrollY()
 
@@ -68,6 +79,7 @@ observer.addCallbacks({
 function onScrollY() {
   scrollY = observer.getScrollY()
   if((mainSlideOffsetTop - scrollY) < (viewportHeight - (mainSlideHeight/2))) showMainSlide();
+  if((colorfulSlideOffsetTop - scrollY) < (viewportHeight - (colorfulSlideHeight/2))) showColorfulSlide();
 }
 
 
@@ -79,4 +91,21 @@ function showMainSlide() {
   setTimeout(function(){taglineElement.classList.add("shown")}, 400)
   setTimeout(function(){emailCaptionElement.classList.add("shown")}, 500)
   setTimeout(function(){headerElement.classList.add("shown")}, 1000)
+}
+
+//showColorfulSlide animation
+var colorfulSlideShown = false
+function showColorfulSlide() {
+  if(colorfulSlideShown) return
+  colorfulSlideShown = true
+
+  for (var i = colorfulSlideBlocksElements.length; i--;){
+    showColorfulSlideBlock(i)
+  }
+}
+
+function showColorfulSlideBlock(i) {
+  setTimeout(function(){
+    colorfulSlideBlocksElements[i].classList.add("shown")
+  }, i*120)
 }
