00b3981d5c0425de5ad0582273216e60a0c77124
andmironov <thatsmironov@gmail.com>
2016-10-05 01:20:07 +0300

    updates

--- prototypes/u/js/main.js
+++ prototypes/u/js/main.js
@@ -1,35 +1,35 @@
 let ScrollOver = require("./lib/ScrollOver.js")
 
+let body = document.querySelectorAll("body")[0]
+
 let tagline = document.querySelectorAll(".tagline")[0]
 let iphone = document.querySelectorAll(".phone--iphone")[0]
 let android = document.querySelectorAll(".phone--android")[0]
 let textBlock = document.querySelectorAll(".text-block")[0]
 
+setTimeout(() => body.classList.add("shown"), 400)
 
 new ScrollOver({
   keyframes : [
-
-
     {
       element : iphone,
-      domain : [0, 500],
+      domain : [0, 600],
       animate: [
         {
           property : "translateY",
-          range : [0, 100]
+          range : [0, 120]
         }
       ]
     },
     {
       element : android,
-      domain : [0, 500],
+      domain : [0, 600],
       animate: [
         {
           property : "translateY",
-          range : [0, 80]
+          range : [0, 90]
         }
       ]
-    },
-
+    }
   ]
 }).init()
