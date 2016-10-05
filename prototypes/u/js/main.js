f1521c18f726be67065e187545c9cac096372187
andmironov <thatsmironov@gmail.com>
2016-10-05 11:15:38 +0300

    added stuff

--- prototypes/u/js/main.js
+++ prototypes/u/js/main.js
@@ -6,27 +6,28 @@ let tagline = document.querySelectorAll(".tagline")[0]
 let iphone = document.querySelectorAll(".phone--iphone")[0]
 let android = document.querySelectorAll(".phone--android")[0]
 let textBlock = document.querySelectorAll(".text-block")[0]
+
 setTimeout(() => body.classList.add("shown"), 400)
 
 new ScrollOver({
   keyframes : [
     {
       element : iphone,
-      domain : [0, 600],
+      domain : [0, 800],
       animate: [
         {
           property : "translateY",
-          range : [0, 120]
+          range : [0, 200]
         }
       ]
     },
     {
       element : android,
-      domain : [0, 600],
+      domain : [0, 800],
       animate: [
         {
           property : "translateY",
-          range : [0, 90]
+          range : [0, 140]
         }
       ]
     }
